a1b108da6a9a525ffeb2e43269daa46193e85b45
reznov9185 <reznov9185@gmail.com>
2021-11-13 18:50:42 -0700

    Launched: Flash app with connected with mysql db - updated readme - added app.py - added requirements.txt for dependencies

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# flask_crud_demo
+# Database Project for DBMS-3:
+### Tech
+
+Build on top a web micro-framwork `Flask`
+
+- [flask] - [web development, one drop at a time](https://flask.palletsprojects.com/en/2.0.x/)
+- [MariaDB] - [mysql installation](https://dev.mysql.com/doc/mysql-installation-excerpt/5.7/en/)
+- [Python3] - [python3](https://www.python.org/downloads/)
+- [Environment] - [Python with pip+venv](https://packaging.python.org/guides/installing-using-pip-and-virtual-environments/)
+
+### Run (Bash)
+
+*-> Pre-requisites*
+
+```sh
+python3 -m venv venv
+```
+```sh
+source venv/bin/activate
+```
+```sh
+pip install -r requirements.txt
+```
+*-> Running webserver in development mode (with hot loading)*
+
+```sh
+export FLASK_ENV=development
+```
+
+To run on a desired port:
+
+```sh
+export FLASK_RUN_PORT=your_desired_port
+```
+*-> Here we go...*
+
+```sh
+flask run
+```
---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+import sys
+import mysql.connector
+from mysql.connector import errorcode
+
+app = Flask(__name__)
+
+@app.route("/")
+def db_connect():
+    try:
+        cnx = mysql.connector.connect(
+                                    host='dbclass.cs.nmsu.edu',
+                                    user='srahman',
+                                    password='test_dbms1',
+                                    database='test')
+        cursor = cnx.cursor(buffered=True)
+        return "Hello, Earthlings... Database connected successfully. :P See: " + str(cursor)  
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            return "Something is wrong with your user name or password"
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            return "Database does not exist"
+        else:
+            return err
